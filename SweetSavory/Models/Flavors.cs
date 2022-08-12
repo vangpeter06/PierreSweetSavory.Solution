5a4f5d6ebba3687743ed53f17275c84e607cc909
Peter Vang <vang_peter06@yahoo.com>
2022-08-12 16:06:28 -0700

    add back to account link on home index

--- SweetSavory/Models/Flavors.cs
+++ SweetSavory/Models/Flavors.cs
@@ -11,6 +11,7 @@ namespace SweetSavory.Models
 
     public int FlavorId { get; set; }
     public string Name { get; set; }
+    public virtual ApplicationUser User { get; set; }
 
     public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
   }
